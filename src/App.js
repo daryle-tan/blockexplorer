31747dd604c2b2b03b6f137a0dffc1d91d9b3a7b
daryle <daryle.tan@gmail.com>
2023-06-24 16:51:00 -0500

    fin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Alchemy, BigNumber, Network } from "alchemy-sdk"
+import { Alchemy, Network } from "alchemy-sdk"
 import { useEffect, useState } from "react"
-
+import logo from "./sherblockLogo.png"
 import "./App.css"
 
 // Refer to the README doc for more information about using API
@@ -22,7 +22,7 @@ function App() {
   const [blockNumber, setBlockNumber] = useState()
   const [blockWithTransaction, setBlockWithTransaction] = useState("")
   const [transactionReceipt, setTransactionReceipt] = useState()
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(99)
 
   useEffect(() => {
     async function getBlockNumbers() {
@@ -51,16 +51,16 @@ function App() {
   }
 
   const handleClick = (i) => {
-    // if (i !== index) {
-    setIndex(i)
-    getTransactionReceipts(transactionReceipt)
-    setTransactionReceipt(transactionReceipt)
-    // }
-    setTimeout(() => {
-      console.log("here70", i)
-      console.log("here71", transactionReceipt)
-      console.log("here72", blockWithTransaction)
-    }, 1000)
+    if (i !== index) {
+      setIndex(i)
+      getTransactionReceipts(transactionReceipt)
+      setTransactionReceipt(transactionReceipt)
+    }
+    // setTimeout(() => {
+    //   console.log("here70", i)
+    //   console.log("here71", transactionReceipt)
+    //   console.log("here72", blockWithTransaction)
+    // }, 1000)
   }
 
   let transaction
@@ -68,7 +68,7 @@ function App() {
     transaction = blockWithTransaction.transactions.map((blox, i) => {
       return (
         <div key={i}>
-          <span className="field">Transaction Hash: </span>
+          <span className="fields">Transaction Hash: </span>
           <button onClick={() => handleClick(i)} id={i}>
             {blox.hash.slice(0, 8) + "..." + blox.hash.slice(-8)}
           </button>
@@ -79,79 +79,91 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <div>
-          <span className="fields">Block Number: {blockNumber}</span>
-        </div>
-        {blockWithTransaction ? (
-          <>
-            <div>
-              {/* <a onClick={handleClick}> */}
-              <span className="fields">Hash: </span>
-              {blockWithTransaction.hash.slice(0, 8) +
-                "..." +
-                blockWithTransaction.hash.slice(-8)}
-              {/* </a> */}
-            </div>
-            <div>
-              <span className="fields">Gas Used: </span>
-              {parseInt(blockWithTransaction.gasUsed)}
-            </div>
-            <div>
-              <span className="fields">Miner: </span>
-              {blockWithTransaction.miner.slice(0, 8) +
-                "..." +
-                blockWithTransaction.miner.slice(-8)}
-            </div>
-            <div>
-              <span className="fields">Nonce: </span>
-              {blockWithTransaction.nonce}
-            </div>
-            <div>
-              <span className="fields">Timestamp: </span>
-              {blockWithTransaction.timestamp}
-            </div>
-          </>
-        ) : null}
+      <div className="container-logo">
+        <img src={logo} className="icon" alt="Sherblock Holmes Logo" />
       </div>
-
-      <div className="BlockWithTransaction">
-        <div className="App2">
-          Block With Transaction:
-          {transaction}
-        </div>
-      </div>
-
-      <div className="oneTransaction">
-        <div className="App3">Transaction Details:</div>
-        {transactionReceipt ? (
-          <>
+      <div id="container">
+        <div className="App">
+          <div className="subApp">
+            <span className="blockNumberHeader">-- Current Block --</span>
             <div>
               <span className="fields">Block Number: </span>
-              {transactionReceipt.blockNumber}
+              {blockNumber}
             </div>
-            <div>
-              <span className="fields">From: </span>
-              {transactionReceipt.from}
-            </div>
-            <div>
-              <span className="fields">To: </span>
-              {transactionReceipt.to}
-            </div>
-            <div>
-              <span className="fields">Status: </span>
-              {transactionReceipt.status}
-            </div>
-            <div>
-              <span className="fields">Gas Used: </span>
-              {parseInt(transactionReceipt.gasUsed)} wei
-            </div>
-            <div>
-              <span className="fields">Gas Price: </span>
-              {parseInt(transactionReceipt.effectiveGasPrice)} wei
-            </div>
-          </>
-        ) : null}
+            {blockWithTransaction ? (
+              <>
+                <div>
+                  <span className="fields">Hash: </span>
+                  {blockWithTransaction.hash.slice(0, 8) +
+                    "..." +
+                    blockWithTransaction.hash.slice(-8)}
+                </div>
+                <div>
+                  <span className="fields">Gas Used: </span>
+                  {parseInt(blockWithTransaction.gasUsed)}
+                </div>
+                <div>
+                  <span className="fields">Miner: </span>
+                  {blockWithTransaction.miner.slice(0, 8) +
+                    "..." +
+                    blockWithTransaction.miner.slice(-8)}
+                </div>
+                <div>
+                  <span className="fields">Nonce: </span>
+                  {blockWithTransaction.nonce}
+                </div>
+                <div>
+                  <span className="fields">Timestamp: </span>
+                  {blockWithTransaction.timestamp}
+                </div>
+              </>
+            ) : null}
+          </div>
+          <div className="subApp">
+            <span className="txDetailsHeader">-- Transaction Details --</span>
+            {transactionReceipt ? (
+              <>
+                <div>
+                  <span className="fields">Block Number: </span>
+                  {transactionReceipt.blockNumber}
+                </div>
+                <div>
+                  <span className="fields">From: </span>
+                  {transactionReceipt.from.slice(0, 8) +
+                    "..." +
+                    transactionReceipt.from.slice(-8)}
+                </div>
+                <div>
+                  <span className="fields">To: </span>
+                  {transactionReceipt.to.slice(0, 8) +
+                    "..." +
+                    transactionReceipt.to.slice(-8)}
+                </div>
+                <div>
+                  <span className="fields">Status: </span>
+                  {transactionReceipt.status}
+                </div>
+                <div>
+                  <span className="fields">Gas Used: </span>
+                  {parseInt(transactionReceipt.gasUsed)} wei
+                </div>
+                <div>
+                  <span className="fields">Gas Price: </span>
+                  {parseInt(transactionReceipt.effectiveGasPrice)} wei
+                </div>
+              </>
+            ) : null}
+          </div>
+        </div>
+
+        <div className="BlockWithTransaction">
+          <div className="App2">
+            <span className="blockWithTxHeader">
+              -- Block with Transactions --
+            </span>
+            {transaction}
+          </div>
+        </div>
       </div>
     </>
   )
